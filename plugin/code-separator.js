a11c36153b62ab857c57618a507602de3b03343e
Mike Schäkermann <m.schaekermann@spontaneousorder.de>
2013-08-29 02:51:45 -0400

    test

--- plugin/code-separator.js
+++ plugin/code-separator.js
@@ -1,6 +1,6 @@
 var handler = function(compileStep) {
 	error({ message: "Parsing file", sourcePath: compileStep.inputPath });
-	compileStep.addJavascipt({
+	compileStep.addJavaScript({
 		path: "program.js",
 		data: "var HARR = 'ding'; alert('BLUB');",
     	sourcePath: "program.js",
