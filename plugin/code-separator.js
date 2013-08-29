079172daeb2ede51d00005df7845fe2edf5f1ff3
Mike Schäkermann <m.schaekermann@spontaneousorder.de>
2013-08-29 02:49:04 -0400

    silly test

--- plugin/code-separator.js
+++ plugin/code-separator.js
@@ -1,7 +1,11 @@
-console.log('Running code-separator');
-
 var handler = function(compileStep) {
-	error({ message: "Parsing file", sourcePath: compileStep.inputPath })
+	error({ message: "Parsing file", sourcePath: compileStep.inputPath });
+	compileStep.addJavascipt({
+		path: "program.js",
+		data: "var HARR = 'ding'; alert('BLUB');",
+    	sourcePath: "program.js",
+    	bare: true
+	});
 }
 
 Plugin.registerSourceHandler("js", handler);
